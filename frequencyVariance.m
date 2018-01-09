a8e2947dbc84276be87c26d0d6c30c6f7ec592d4
cndnb <blinnc@uw.edu>
2018-01-09 14:04:35 -0800

    Frequency variance bugfix
    Would sometimes send a matrix of the incorrect size to the OLS function which caused a rank not equal to number of columns error. freqVarTest checks how many points are affected by the fix.

--- frequencyVariance.m
+++ frequencyVariance.m
@@ -9,17 +9,22 @@ for counter=1:stepSize:length(data(:,1))
     if (counter+stepSize > length(data(:,1)))
       modStep = length(data(:,1))-counter;
     endif
-    
+    if (modStep >10)
     %OLS fit to specific chunk
     designX = genSineSeed(data(counter:(counter+modStep),1),inFreq);
     [sChkBeta, sChkSigma, sChkR, sChkErr, sChkCov] = ols2(data(counter:(counter+modStep),2),designX);
-    %I think this works
+    %Prevents divide by zero errors
     if (sChkSigma == 0)
       sChkSigma = 1;
     endif
     %Assigns variance to all points in the chunk
     sineSTDev(counter:(counter+modStep),1) = sChkSigma.*ones(modStep+1,1);
     modStep=stepSize;
+    else
+    sineSTDev(counter:(counter+modStep),1) = 1.*ones(modStep+1,1);
+    modStep = stepSize;
+    endif
+    
 endfor
 
 %Returns the completed array of variances
