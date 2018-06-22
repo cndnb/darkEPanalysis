7f9096ee1736525a6aedc0b025887295c0008427
Carson <blinnc@uw.edu>
2018-06-21 17:19:08 -0700

    Import sorts data into cells with day length

--- importfakeDarkEP.m
+++ importfakeDarkEP.m
@@ -1,4 +1,5 @@
 d = load('fakeDarkEPAugust92017.dat');
+fullLength = rows(d);
 
 I = 378/(1e7);                                                                    
 f0 = 1.9338e-3;                                                                 
@@ -7,16 +8,17 @@ T = 273+24;
 kappa = (2*pi*f0)^2 * I;
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%% DRIFT CORRECTION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%% DRIFT CORRECTION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%Removes drift from the data
-linearDesign = [ones(length(d(:,1)),1),d(:,1)];
-[lDB, lDS, lDR, LDEr, lDCov] = ols2(d(:,2),linearDesign);
-oldDriftFix = [d(:,1),d(:,2)-(linearDesign*lDB)];
+%%Removes drift from the data
+%linearDesign = [ones(length(d(:,1)),1),d(:,1)];
+%[lDB, lDS, lDR, LDEr, lDCov] = ols2(d(:,2),linearDesign);
+%oldDriftFix = [d(:,1),d(:,2)-(linearDesign*lDB)];
+%
+%%Removes the daily swing from the data
+%[dailyBeta, dailySigma, dailyR] = sineFitter(oldDriftFix(:,1),oldDriftFix(:,2),1/86400);
+%oldDriftFix = [oldDriftFix(:,1),oldDriftFix(:,2)-genSineSeed(oldDriftFix(:,1),1/86400)*dailyBeta];
 
-%Removes the daily swing from the data
-[dailyBeta, dailySigma, dailyR] = sineFitter(oldDriftFix(:,1),oldDriftFix(:,2),1/86400);
-oldDriftFix = [oldDriftFix(:,1),oldDriftFix(:,2)-genSineSeed(oldDriftFix(:,1),1/86400)*dailyBeta];
 
 %%%%%%%%%%%%%%%%%%%%%%%% EARTHQUAKE REMOVAL %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -24,20 +26,27 @@ oldDriftFix = [oldDriftFix(:,1),oldDriftFix(:,2)-genSineSeed(oldDriftFix(:,1),1/
 threshold = 1e-13;
 areaRemove = 10000;
 
-calcTorque = torque(oldDriftFix, I, kappa);
-[driftFix, editTorque,pointsRemoved] = removeEarthquakes(oldDriftFix,calcTorque,threshold,areaRemove);
-check = psd(editTorque(:,1),editTorque(:,2));
+calcTorque = torque(d, I, kappa);
+[driftFix,editTorque] = removeEarthquakes(d,calcTorque,threshold,areaRemove);
+%check = psd(editTorque(:,1),editTorque(:,2));
 
+
+
+for count = 1:rows(driftFix)
+  fullData = [fullData; driftFix{count,1}];
+endfor
 
 
 figure(1);
-plot(oldDriftFix(:,1),oldDriftFix(:,2),driftFix(:,1),driftFix(:,2)+2e-5);
+plot(d(:,1),d(:,2));
+figure(2);
+plot(fullData(:,1),fullData(:,2));
 
 %%Checking that FFT of torque has no peaks
 %figure(1);
 %loglog(check(:,1),check(:,2));
 %
-%Checking the threshold level
-figure(2);
-plot(calcTorque(3:(1e6 - 2),1),calcTorque(3:(1e6 - 2),2),calcTorque(3:(1e6 - 2),1),...
-threshold.*ones(length(calcTorque)-4,1));
+%%Checking the threshold level
+%figure(2);
+%plot(calcTorque(3:(1e6 - 2),1),calcTorque(3:(1e6 - 2),2),calcTorque(3:(1e6 - 2),1),...
+%threshold.*ones(length(calcTorque)-4,1));
--- removeEarthquakes.m
+++ removeEarthquakes.m
@@ -1,33 +1,71 @@
-function [theta,tau,pointsRemove] = removeEarthquakes(data,torque,threshold,areaRemove)
+function [theta,tau] = removeEarthquakes(data,torque,threshold,areaRemove)
+%This method works as long as areaRemove is less than one day.
 
-%Sets up for removal of high torque points.
-noTorqueDisplace = data;
+
+dayLength = 86400; %seconds
+numDays = floor(rows(data)/dayLength);
+dataDivisions = cell(numDays,1);
+
+for count = 0:numDays-1
+  dataDivisions(count+1,1) = data((count*dayLength+1):((count+1)*dayLength),:);
+endfor
+
+%Prepares torque array to have points removed.
 noEarthTorque = torque;
-count = 0;
+
 %Goes through each point to check if torque is above threshold, sets points
 %over threshold to zero
-i=rows(torque);
-while (i>0)
-  if (abs(torque(i,2))>(threshold))
-    %Removes all points within areaRemove of the earthquake point
-    back = (i-areaRemove);
-    forward = (i+areaRemove);
-    if ((i-areaRemove)<0)
-      back = 1;
+for count = 1:numDays
+  aMatrix = dataDivisions{count,1};
+  indS = aMatrix(rows(aMatrix),1);
+  while (indS>aMatrix(1,1)-1)
+    if (abs(torque(indS,2))>(threshold)) %If torque at time exceeds threshold
+      %Removes all points within areaRemove of the earthquake point
+      
+      %Displacement points removal
+      back = ((indS-aMatrix(1,1))-areaRemove);
+      forward = ((indS-aMatrix(1,1))+areaRemove);
+      if (back < 1) %If removal area leaks into previous day
+        try %Attempt to remove points from previous day
+          subMatrix = dataDivisions{count - 1,1}; %Accesses next day
+          subMatrix(rows(subMatrix)+back:rows(subMatrix),:) = []; %Back would be negative, so index = rows(subMatrix)+back
+          dataDivisions{count - 1,1} = subMatrix;
+          back = 1;
+        catch %Otherwise matrix is the first day, do nothing.
+          back = 1;
+        end_try_catch
+      endif
+      if (forward > (rows(aMatrix))) %If removal area leaks into next day
+        try
+          subMatrix = dataDivisions{count + 1,1};
+          subMatrix(1:(forward - rows(aMatrix)),:) = [];
+          dataDivisions{count + 1,1} = subMatrix;
+          forward = rows(aMatrix);
+        catch
+          forward = rows(aMatrix);
+        end_try_catch
+      endif
+      aMatrix(back:forward,:) = [];
+      dataDivisions{count,1} = aMatrix;
+      
+      %Removal on torque array
+      back = indS - areaRemove;
+      forward = indS + areaRemove;
+      if (back < 1)
+        back = 1;
+      endif
+      if (forward > rows(noEarthTorque))
+        forward = rows(noEarthTorque);
+      endif 
+      noEarthTorque(back:forward,2) = 0;
+      indS = back;
     endif
-    if ((i+areaRemove)>length(noTorqueDisplace))
-      forward = length(noTorqueDisplace);
-    endif
-    noTorqueDisplace(back:forward,:) = [];
-    noEarthTorque(back:forward,2) = 0;
-    i = back;
-  endif
-  i = i - 1;
-endwhile
+    indS = indS - 1;
+  endwhile
+endfor
 
-
-%Returns edited array
-pointsRemove = count;
-theta = noTorqueDisplace;
+%Returns edited arrays
+theta = dataDivisions;
 tau = noEarthTorque;
+
 endfunction
