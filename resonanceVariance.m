71344408e483a4985f0a1d316e0a2b1c6beed902
Carson Blinn <blinnc@uw.edu>
2018-07-12 16:27:17 -0700

    Testing and changes to resonanceVariance in attempt to find peaks in fake data set

--- resonanceVariance.m
+++ resonanceVariance.m
@@ -1,6 +1,7 @@
-function ret = resonanceVariance(data, chunkLength)
+function [ret,removeDrift] = resonanceVariance(data, chunkLength)
   %Resonant frequency of pendulum
-  f0 = 1.9338e-3;                                                                 
+  f0 = 1.9338e-3;
+  fEarth = 1/86400;                                                               
   %Step size is chunk length periods of the frequency
   stepSize = ceil(chunkLength*(1/f0));
   %Number of whole chunks that fit evenly into the data
@@ -12,23 +13,28 @@ function ret = resonanceVariance(data, chunkLength)
     stepSize = rows(data);
     endValue = rows(data);
   endif
-  
+  %Removes drift from data for better weighting
+  tM = data(:,1);
+  rX = [ones(rows(data),1),tM,sin((2*pi*fEarth).*tM),cos((2*pi*fEarth).*tM)];
+  [rB,rS,rR,rERR,rCOV] = ols2(data(:,2),rX);
+  removeDrift = [data(:,1),data(:,2)-rX*rB];
+    
   %Initializes design matrix for resonant sine wave
-  designX = [sin((2*pi*f0).*(1:stepSize)'), cos((2*pi*f0).*(1:stepSize)')];
+  designX = [sin((2*pi*f0).*data(:,1)), cos((2*pi*f0).*data(:,1))];
   %Initializes accumulation array
   weightVal = ones(rows(data),1);
   %For each chunk calculate the chi squared and save it.
   for count = 1:evenDivisible
-    [b,s,r,err,cov] = ols2(data(stepSize*(count-1) + 1:stepSize*count,2),designX);
+    [b,s,r,err,cov] = ols2(removeDrift(stepSize*(count-1) + 1:stepSize*count,2),designX(stepSize*(count-1) + 1:stepSize*count,:));
     weightVal(stepSize*(count-1) + 1:stepSize*count,1) = s*ones(stepSize,1);
   endfor
   %If the analysis stops before the end of the array, extend last known value
   if (endValue < rows(data))
-    weightVal(endValue:end) = weightVal(stepSize*evenDivisible,1)*ones(rows(data)-endValue,1);
+    weightVal(endValue:end) = weightVal(stepSize*evenDivisible-1,1)*ones((rows(data)-endValue) + 1,1);
   endif
   
   %Weights are equal to 1/chisquared
-  ret = 1 ./ weightVal;
+  ret = weightVal;
 endfunction
 
 %!test
@@ -39,14 +45,22 @@ endfunction
 %! for count = 1:rows(varRes)
 %!  assert(varRes(count) != 0) %Checks that the weight is never zero
 %! endfor
-%! endfor
 
 %!test
 %! b=1:10000;
 %! b=b';
 %! b=[b,randn(10000,1)]; %Data array time 1-10000, random y values
 %! f0 = 1.9338e-3;
-%! [B,olsSigma,R,Err,Cov] = ols2(b(:,2),createSineComponents(b(:,1),fitFreq(num))); %Finds chi square of fit
-%! varRes = resonanceVariance(b,rows(b)*fitFreq(num)); %Chunk Length is equal to length of data
+%! fEarth = 1/86400;
+%! 
+%! t = b(:,1);
+%! tX = [ones(rows(b),1),t,sin((2*pi*fEarth).*t),cos((2*pi*fEarth).*t)];
+%! [tB,tS,tR,tERR,tCOV] = ols2(b(:,2),tX);
+%! removeDrift = [b(:,1),b(:,2)-tX*tB];
+%! 
+%! designX = [sin((2*pi*f0).*b(:,1)), cos((2*pi*f0).*b(:,1))];
+%! [B,olsSigma,Err,Cov] = ols2(removeDrift(:,2),designX); %Finds chi square of fit
+%! [varRes,cB] = resonanceVariance(b,rows(b)*f0); %Chunk Length is equal to length of data
 %! fVSigma = varRes(1); %Takes first point, all points of fV should be the same
-%! assert (fVSigma == (1./olsSigma))
+%! assert (fVSigma,olsSigma)
+
